b06ac0d9bc5d92ae8a138a5be73ad6df6070abf0
Augustin Bielefeld <augustinbielefeld@gmail.com>
2019-11-16 10:38:45 +0100

    merci de ranger le matériel :)

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,10 @@
 #include "motor.h"
 #include "PIDv2.h"
 
-Encoder encoder(A_1, B_1); //Déclaration des pins de l'encodeur
-int32_t position = 0;
-
-Motor motor(IN1_1, IN2_1);
-
 void setup() {
-	Serial.begin(9600);
-	delay(3000);
-	motor.set_pwm(28);
+
 }
 
 void loop() {
-	position = encoder.read();
-	Serial.println(position);
-	delay(100);
+
 }

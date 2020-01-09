11f1ecd8320bb0e3e856590927283f729ab141e0
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2020-01-09 14:49:33 +0100

    Added Lidar code

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,11 @@
-#include <Arduino.h>
-#include "board.h"
-#include "locomotion.h"
-#include "binserial.h"
+#include "lidar.h"
 
-const uint32_t sample_time = 10;
-uint32_t time; // Temps de la dernière période d'échantillonnage
-float distance;
-
-Locomotion locomotion(sample_time/1000.);
+Lidar lidar;
 
 void setup() {
-    Serial.begin(9600); // Initialise Serial communication
-    while (!Serial); // Attend que la liaison soit établie
 
-    // Initialise LED de debug
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    time = millis() - sample_time; // Initialise le temps
 }
 
 void loop() {
-    // Éxécute les instruction toutes les périodes d'échantillonnages
-    if (millis() - time >= sample_time) {
-        time = millis();
-
-        // Run the locomotion
-        if (STOP == locomotion.run())
-            digitalWriteFast(LED_BUILTIN, HIGH);
-        else
-            digitalWriteFast(LED_BUILTIN, LOW);
-
-        writeData(locomotion.getPosition(), sizeof(position_t));
-        if (Serial.available()) {
-            readData(&distance, sizeof(distance));
-            locomotion.translateFrom(distance);
-        }
-    }
+  lidar.run();
 }
